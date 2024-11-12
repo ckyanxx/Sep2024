273004dd5482061cd658e578450d423209c97c3a
ckyanxx <chengkahyan05@gmail.com>
2024-11-12 12:15:36 +0800

    Add files via upload

---
+++ project/WD/mainjava.js
@@ -0,0 +1,14 @@
+﻿//when scroll down
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        document.getElementById("bar").style.padding = "8px 10px";
+        document.getElementById("logo").height = "35px";
+        document.getElementById("logo").width = "50px";
+    } else {
+        document.getElementById("bar").style.padding = "30px 10px";
+        document.getElementById("logo").height = "85px";
+        document.getElementById("logo").width = "100px";
+    }
+}
